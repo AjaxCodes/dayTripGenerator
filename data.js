796092ff81e1f6ef32cacfcd7405e305b4f7a4d2
unknown <adamjsteinbacher@gmail.com>
2020-09-09 21:53:41 -0500

    variables that will be represented by arrays created

---
+++ data.js
@@ -0,0 +1,11 @@
+data.js
+
+let destination [milwaukee, chicago, dallas, tampa, orlando, miami, losAngelas];
+
+let resturaunt [crackerBarrel, bobEvans, hardees, fancySteakhouse, sushiBar,];
+
+let transportation [plane, train, automobile];
+
+let entertainment [movie, barCrawl, tour, artMuseum, museum, artGallary, worldMostFamousWhatever]
+
+Math.random();
