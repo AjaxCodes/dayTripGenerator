d0fd1731b54654ae6d621e5c4310ab6e39c2a38a
unknown <adamjsteinbacher@gmail.com>
2020-09-10 15:24:42 -0500

    findDestination function completed and tested.

--- data.js
+++ data.js
@@ -1,11 +1,32 @@
-data.js
+'use strict'
+var destination = ['milwaukee', 'chicago', 'dallas', 'tampa', 'orlando', 'miami', 'los Angelas'];
+var resturaunt  = ['cracker Barrel', 'bobEvans', 'hardees', 'fancySteakhouse', 'sushiBar'];
+var transportation  = ['plane', 'train', 'automobile'];
+var entertainment  = ['movie', 'barCrawl', 'tour', 'artMuseum', 'museum', 'artGallary', 'worldMostFamousWhatever']
+let result;
 
-let destination [milwaukee, chicago, dallas, tampa, orlando, miami, losAngelas];
+function findDestination(){
+   let result = Math.floor(Math.random() * destination.length);
+   result = destination[result];
+    console.log(result);
+}
+findDestination();
 
-let resturaunt [crackerBarrel, bobEvans, hardees, fancySteakhouse, sushiBar,];
+//function findResturaunt (){
+   // for (i = 0; resturaunt.length; i++)
+   // resturaunt.Math.random + 1();
 
-let transportation [plane, train, automobile];
+   // console.log();
+//}
 
-let entertainment [movie, barCrawl, tour, artMuseum, museum, artGallary, worldMostFamousWhatever]
+//function findTransportation(){
+   // for (i = 0; transportation.length; i++)
+//transportatiopn.Math.random + 1();
+   // console.log();
+//}
 
-Math.random();
+//function findEntertainment(){
+   // for (i = 0; entertainment.length; i++)
+    //entertainment.Math.random  + 1();
+   // console.log();
+//}
