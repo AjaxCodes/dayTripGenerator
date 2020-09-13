4a866c81098515bf0c8aef0cd4802560078ae55b
unknown <adamjsteinbacher@gmail.com>
2020-09-13 18:55:59 -0500

    started on cube for index.html. created and tested friend function. switchcase tested and working, for reselect function. userstories commplete.

--- data.js
+++ data.js
@@ -6,10 +6,11 @@
 //After everything is done, print the completed trip
 
 
-var destination = ['Narnia', 'Bikini Bottom', 'Middle Earth', 'The Shire', 'New Asgard', 'Wakanda', 'Geonosis'];
+var destination = ['Narnia', 'Bikini Bottom', 'Middle Earth', 'Flavortown', 'The Shire', 'New Asgard', 'Wakanda', 'Geonosis'];
 var resturaunt  = ['Chum Bucket', 'Bobs Burger Shack', 'The Crusty Crab', 'The Hawthorne Grille', 'Shalom Grill'];
 var transportation  = ['Plane', 'Train', 'Automobile'];
 var entertainment  = ['Movie', 'Bar Crawl', 'Tour', 'Art Museum', 'Jelly Fishing', 'Museum', 'Art Gallary', 'Worlds Most Famous Whatever'];
+var friend = ['Dr Strange', 'Spongebob', 'Voldomort' , 'Buster Douglas', 'Mike Tyson ', 'Guy Fieri']
 var trip = [];
 let result;
 
@@ -19,8 +20,7 @@ function findDestination(){
     console.log(chosenDestination);
    return chosenDestination;
 }
-let chosenDestination = findDestination();
-trip[0] = (chosenDestination);
+
 
 //
 
@@ -30,8 +30,7 @@ function findResturaunt (){
   console.log(chosenResturaunt);
   return chosenResturaunt;
 }
-let chosenResturaunt = findResturaunt();
-trip[1] = (chosenResturaunt);
+
 
 // 
 
@@ -41,8 +40,7 @@ function findTransportation(){
   console.log(chosenTransportation);
   return chosenTransportation;
 }
-let chosenTransportation = findTransportation();
-trip[2] = (chosenTransportation);
+
 
 //
 
@@ -52,49 +50,84 @@ let chosenEntertainment = entertainment[result];
    console.log(chosenEntertainment);
    return chosenEntertainment;
 }
-let chosenEntertainment = findEntertainment();
-trip[3] = (chosenEntertainment);
+function findFriend(){
+   let result = Math.floor(Math.random() * friend.length);
+let chosenfriend = friend[result];
+   console.log(chosenfriend);
+   return chosenfriend;
+}
 
 //
 
 function reSelectOption(){
-  let  user = prompt('If everything is to your liking select 5,  If you would like to change anything else, select 1 for new destination. select 2 for new resturaunt. Select, 3 for new mode of transportation. Select, 4 for new entertainment options.');
+   console.log(trip);
+  let  user = prompt('If everything is to your liking Type: 6 If you would like to change anything else Type:1, for new destination. Type: 2, for new resturaunt. Type: 3, for new mode of transportation. Type: 5, for new entertainment options.').toLowerCase();
   switch (user){
       
-     case 1:
-        choice = "Destination";
-        let chosenDestination = findDestination();
-        trip[0](chosenDestination);
+     case '1':
+         trip[0]=findDestination();
+        console.log(trip);
+        reSelectOption();
+       break;
+     case '2':
+         trip[1]=findResturaunt();
         console.log(trip);
         reSelectOption();
       break;
-     case 2:
-        choice = "resturaunt";
-        let chosenResturaunt = findResturaunt();
-        trip[1](chosenResturaunt);
-        console.log(trip);
-        reSelectOption();
-       break;
-      case 3:
-         choice = "transportation";
-         let chosenTransportation = findTransportation();
-         trip[2](chosenTransportation);
+      case '3':
+         trip[2]=findTransportation();
          console.log(trip);
          reSelectOption();
-       break;
-      case 4:
-          choice = "entertainment";
-          let chosenEntertainment =  findEntertainment();
-          trip[3](chosenEntertainment);
+          break;
+      case '4':
+         trip[3]=findEntertainment();
           console.log(trip);
           reSelectOption();
        break;
-      case 5:
-         choice = "This sounds Fun. Im all complete here.";
+       case '5':
+          trip[4]=findFriend;
          console.log(trip);
-         console.log('complete');
-       break;
+         reSelectOption();
+         break;
+      case '6':
+         console.log(trip,'complete');
+         break;
+      default:
+         reSelectOption(); // restart app
+      break;
+   }
   }
-  
-}
+
+
+function confirmResults(){
+   let  user = prompt('If everything is to your liking Type: Excelcior!  if you would like to change another option, please Type: Yes').toLowerCase();
+   switch (user){
+       
+      case 'yes':
+         reSelectOption();
+     break;
+       case 'excelcior':
+          console.log('complete');
+          return trip,'complete';
+        break;
+       default:
+         confirmResults(); // restart app
+       break;
+    }
+   }
+
+
+
+let chosenDestination = findDestination();
+trip[0] = (chosenDestination);//push()
+let chosenResturaunt = findResturaunt();
+trip[1] = (chosenResturaunt);
+let chosenTransportation = findTransportation();
+trip[2] = (chosenTransportation);
+let chosenEntertainment = findEntertainment();
+trip[3] = (chosenEntertainment);
+let chosenFriend = findFriend();
+trip[4] = (chosenFriend);
 reSelectOption();
+
+confirmResults();
